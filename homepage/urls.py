75eab57f87d432259cbf1198078c7a49688b3088
Jacob Waldrip <jacob.waldrip@egov.com>
2019-12-06 13:47:40 -0600

    made it on click change and added additional options

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
     path('send-email-on-download', views.email_on_download, name='email_on_download'),
-    path('kris-facts', views.kris_facts, name='kris_facts')
+    path('kris-facts', views.kris_facts, name='kris_facts'),
+    path('get-kris-fact', views.get_kris_fact, name='get_kris_fact')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -2,9 +2,65 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 import time
 import random
 
+# Return a random Kris Fact
+facts = [
+    "Norwood? More like Nandwood",
+    "Did you mean 'Chris'?",
+    "Did you know Kris is an endangered species?",
+    "Go Cowboys",
+    "Kris: At least he's not Logan",
+    "Did you know twenty-two billion billion billion Kris's can fit inside the sun? And we don't even want one of them!",
+    "Have you ever noticed how gnome-like and stupid he is?",
+    "Yeah I can't go to lunch, I just got here, can you bring me something back thoooo?",
+    "I'm not gnome like though, more dwarf without the beard",
+    "Kris enjoys: cat'ing out logs in his terminal to look busy.",
+    "Kris is unable to watch Rick and Morty due to his dwindling IQ",
+    "Kris is a good man",
+    "Hold my diapers!",
+    "Kris's bulking season is now dubbed 'Kris-mass'",
+    "As Krispy as a Krispy Cracker",
+    "Why isn't this working? *Slams Desk* uwu'",
+    "The body does not turn because the face is not confident'",
+    "Woooo!",
+    "I love Tuckers",
+    "I wish I was on Team Vector!",
+    "I hate breast cancer supporters.",
+    "Rest in peps",
+    "I like my women like I like my coffee. Dark and bitter.",
+    "You guys don't?",
+    "How often do men throw money at me? Often enough.",
+    "I'm rebooting the server guys give me a second.",
+    "Don't talk about my body, I don't have any armor.",
+    "I'm weak-willed and thin-skinned.",
+    "I love the new Carly Rae Jepsin album. It slaps!",
+    "That's too bad.",
+    "Bruuuuuh",
+    "I love Gabe. He has Cooper's hair.",
+    "I'm starving. Let's go eat!",
+    "I don't like almonds, but I'm a fan of the almond brothers.",
+    "That's a good point, I didn't think about that.",
+    "That's fair.",
+    "Ahhh yeah that's right!",
+    "Interesting...",
+    "YIP",
+    "Me big dumb",
+    "I don't pay attention to me.",
+    "Sorry, I was moonlighting.",
+    "Yes, that's the joke.",
+    "I agree with that.",
+    "Who wants to go to Oily Mikes?",
+    "I hate linear algebra with a burning passion.",
+    "Jacob is a good man.",
+    "Jacob is chaotic good.",
+    "Logan is chaotic evil.",
+    "Logan is not a good man.",
+    "Jasmin is lawful neutral.",
+    "Cooper is a good man."
+]
 
 # Create your views here.
 
@@ -36,49 +92,21 @@ def kris_facts(request):
         return HttpResponse(status=400)
     if security_key != '1234':
         return HttpResponse(status=403)
-    
-    # Return a random Kris Fact
-    facts = [
-        "Norwood? More like Nandwood",
-        "Did you mean 'Chris'?",
-        "Did you know Kris is an endangered species?",
-        "Go Cowboys",
-        "Kris: At least he's not Logan",
-        "Did you know twenty-two billion billion billion Kris's can fit inside the sun? And we don't even want one of them!",
-        "Have you ever noticed how gnome-like and stupid he is?",
-        "Yeah I can't go to lunch, I just got here, can you bring me something back thoooo?",
-        "I'm not gnome like though, more dwarf without the beard",
-        "Kris enjoys: cat'ing out logs in his terminal to look busy.",
-        "Kris is unable to watch Rick and Morty due to his dwindling IQ",
-        "Kris is a good man",
-        "Hold my diapers!",
-        "Kris's bulking season is now dubbed 'Kris-mass'",
-        "As Krispy as a Krispy Cracker",
-        "Why isn't this working? *Slams Desk* uwu'",
-        "The body does not turn because the face is not confident'",
-        "Woooo!",
-        "I love Tuckers",
-        "I wish I was on Team Vector!",
-        "I hate breast cancer supporters.",
-        "Rest in peps",
-        "I like my women like I like my coffee. Dark and bitter.",
-<<<<<<< HEAD
-        "You guy's don't?"
-=======
-        "You guy's don't?",
->>>>>>> e7e6d24497dad848aad0506890564811478262be
-        "How often do men throw money at me? Often enough.",
-        "I'm rebooting the server guys give me a second.",
-        "Don't talk about my body, I don't have any armor.",
-        "I'm weak-willed and thin-skinned.",
-        "I love the new Carly Rae Jepsin album. It slaps!",
-        "That's too bad.",
-        "Bruuuuuh",
-        "I love Gabe. He has Cooper's hair.",
-        "I'm starving. Let's go eat!",
-        "I don't like almonds, but I'm a fan of the almond brothers."
-    ]
 
     random_fact = random.choice(facts)
 
     return render(request, 'kris.html', {'app_url': app_url, 'fact': random_fact })
+
+@csrf_exempt
+def get_kris_fact(request):
+
+    app_url = request.path
+    if request.method == 'GET' and 'key' in request.GET:
+        security_key = request.GET['key']
+    else:
+        return HttpResponse(status=400)
+    if security_key != '1234':
+        return HttpResponse(status=403)
+
+    random_fact = random.choice(facts)
+    return JsonResponse({'fact': random_fact})
