7fc0092f85e413f1d8ff222b92ae916f4bcfea45
Jacob Waldrip <jacob.waldrip@egov.com>
2019-12-22 18:57:25 -0400

    completed table for creep roles, started js for it

---
+++ static/js/creep-role.js
@@ -0,0 +1,12 @@
+$(document).ready(function (){
+    
+    // Miner Click Event
+    $('#miner-row').click(function() {
+        console.log("Miner row selected");
+    });
+
+    // Harvester Click Event
+    $('#harvester-row').click(function() {
+        console.log("Harvester row selected");
+    });
+});
