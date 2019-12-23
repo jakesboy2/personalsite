fd8175b5537e3b981e6a8b16facdf704bec76b15
Jacob Waldrip <jacob.waldrip@egov.com>
2019-12-22 22:20:24 -0400

    added description for original creeps, and added click events for all rows

--- static/js/creep-role.js
+++ static/js/creep-role.js
@@ -9,4 +9,96 @@ $(document).ready(function (){
     $('#harvester-row').click(function() {
         console.log("Harvester row selected");
     });
+
+    // Miner Click Event
+    $('#lorry-row').click(function() {
+        console.log("Lorry row selected");
+    });
+
+    // Harvester Click Event
+    $('#claimer-row').click(function() {
+        console.log("claimer row selected");
+    });
+
+    // Miner Click Event
+    $('#mineral-row').click(function() {
+        console.log("mineral row selected");
+    });
+
+    // Harvester Click Event
+    $('#power-row').click(function() {
+        console.log("power row selected");
+    });
+
+    // Miner Click Event
+    $('#r-miner-row').click(function() {
+        console.log("remote miner row selected");
+    });
+
+    // Harvester Click Event
+    $('#r-harvester-row').click(function() {
+        console.log("remote harvester row selected");
+    });
+
+    // Miner Click Event
+    $('#r-reserver-row').click(function() {
+        console.log("remote reserver row selected");
+    });
+
+    // Harvester Click Event
+    $('#r-colonizer-row').click(function() {
+        console.log("remote colonizer row selected");
+    });
+
+    // Miner Click Event
+    $('#worker-row').click(function() {
+        console.log("worker row selected");
+    });
+
+    // Harvester Click Event
+    $('#manager-row').click(function() {
+        console.log("manager row selected");
+    });
+
+    // Miner Click Event
+    $('#d-defender-row').click(function() {
+        console.log("domestic defender row selected");
+    });
+
+    // Harvester Click Event
+    $('#r-defender-row').click(function() {
+        console.log("remote defender row selected");
+    });
+
+    // Miner Click Event
+    $('#zealot-row').click(function() {
+        console.log("zealot row selected");
+    });
+
+    // Harvester Click Event
+    $('#stalker-row').click(function() {
+        console.log("stalker row selected");
+    });
+
+    // Miner Click Event
+    $('#medic-row').click(function() {
+        console.log("medic row selected");
+    });
+
+    // Harvester Click Event
+    $('#drainer-tank-row').click(function() {
+        console.log("drainer tank row selected");
+    });
+
+    // Miner Click Event
+    $('#drainer-medic-row').click(function() {
+        console.log("drainer medic row selected");
+    });
+
+    // Harvester Click Event
+    $('#scout-row').click(function() {
+        console.log("scout row selected");
+    });
+
+    
 });
